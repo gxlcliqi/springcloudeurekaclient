7a062b71f5795e231e3d9bf9ca6a40cbe6e12591
gxlcliqi <gxlcliqi>
2017-01-05 19:40:25 +0800

    Initial commit

---
+++ src/main/java/com/example/ServiceInstanceRestController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/1/5.
+ */
+@RestController
+public class ServiceInstanceRestController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        List<ServiceInstance> instances = this.discoveryClient.getInstances(applicationName);
+        System.out.println(instances.get(0).getUri());
+        return instances;
+    }
+}
